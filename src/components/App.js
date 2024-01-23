32b1f7eb33815834814c3eed32aaeb0ca6cdb0b1
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-01-23 07:54:44 +0530

    check

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,55 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
-const App = () => {
+export default function App() {
+  let [gender, setGender] = useState("Male");
   return (
     <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+      <h1>Select your gender:</h1>
+      <input
+        onClick={(e) => setGender(e.target.value)}
+        type="radio"
+        id="male"
+        name="gender"
+        value="male"
+      />
+      <label for="male">Male</label>
+      <input
+        onClick={(e) => setGender(e.target.value)}
+        type="radio"
+        id="female"
+        name="gender"
+        value="female"
+      />
+      <label for="female">Female</label>
+      {/* Do not remove the main div */}
 
-export default App
+      {gender === "male" ? (
+        <>
+          <h2 for="shirts">Select your shirt size:</h2>
+          <select name="size" id="size">
+            <option value="Select size" disabled>
+              Select size
+            </option>
+            <option value="small">Small</option>
+            <option value="medium">Medium</option>
+            <option value="large">Large</option>
+          </select>
+        </>
+      ) : (
+        <>
+          <h2 for="dress">Select your dress size:</h2>
+          <select name="size" id="size">
+            <option value="Select size" disabled>
+              Select size
+            </option>
+            <option value="two">2</option>
+            <option value="four">4</option>
+            <option value="six">6</option>
+          </select>
+        </>
+      )}
+    </div>
+  );
+}
